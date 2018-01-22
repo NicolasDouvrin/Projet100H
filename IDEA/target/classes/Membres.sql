218ce2a09933970820d0394253ccfafc5a9a097f
Nicolas Douvrin <nicolas.douvrin@hei.yncrea.fr>
2018-01-22 11:46:56 +0100

    Idea maj 3 compte fonctionnel

--- IDEA/target/classes/Membres.sql
+++ IDEA/target/classes/Membres.sql
@@ -5,7 +5,8 @@ CREATE TABLE `MEMBRE` (
   `email` VARCHAR(42) NOT NULL,
   `nom` VARCHAR(42) NOT NULL,
   `prenom` VARCHAR(42) NOT NULL,
-  `mdp` VARCHAR(42) NOT NULL,
   `classe` VARCHAR(42) NOT NULL,
+  `mdp` VARCHAR(42) NOT NULL,
   PRIMARY KEY (`email`)
 )
+INSERT INTO membre VALUE ('admin@hei.yncrea.fr','admin','admin','H00''hei2018')
